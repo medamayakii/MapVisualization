9528c53ec78ac53485bb8c43d4e9093b44e40c69
medamayakii <parissg0717@gmail.com>
2022-09-21 16:57:27 +0900

    modified loop expression

--- map-1.jl
+++ map-1.jl
@@ -8,14 +8,14 @@ function discrete_colorscale(boundary_vals, mycolors)
     #boundary_vals - vector of values bounding intervals/ranges of interest
     #colors - list of rgb or hex colorcodes for values in [bvals[k], bvals[k+1]],1<=k < length(boundary_vals)
     #returns the plotly  discrete colorscale
-    if length(boundary_vals) != length(mycolors)+1
-           error("length of boundary values should be equal to  length(mycolors)+1")
+    if length(boundary_vals) != length(mycolors) + 1
+        error("length of boundary values should be equal to  length(mycolors)+1")
     end
     bvals = sort(boundary_vals)
-    nvals = [(v-bvals[1])/(bvals[end]-bvals[1]) for v in bvals]  #normalized values
-    
+    nvals = [(v - bvals[1]) / (bvals[end] - bvals[1]) for v in bvals]  #normalized values
+
     dcolorscale = [] #discrete colorscale
-    for k in 1:length(mycolors)
+    for k in eachindex(mycolors)
         append!(dcolorscale, [[nvals[k], mycolors[k]], [nvals[k+1], mycolors[k]]])
     end
 
@@ -26,26 +26,26 @@ url = "https://www.geospatial.jp/ckan/dataset/5afd388d-1e6e-4505-91bc-064391a315
 #df = GeoDataFrames.read("13tokyo500m.geojson")
 df = GeoDataFrames.read(url)
 const n_clusters = 6
-df.cluster = string.((0:size(df)[1]-1).% n_clusters)
+df.cluster = string.((0:size(df)[1]-1) .% n_clusters)
 c = ["#ff0000", "#ffa700", "#afff00", "#08ff00", "#00ff9f", "#00b7ff", "#0010ff", "#9700ff", "#ff00bf", "#ff0000"]
 dcolorscale = discrete_colorscale(Vector(0:n_clusters), c[1:n_clusters])
 
 #refer to https://discourse.julialang.org/t/valid-geojson-object-for-choroplethmapbox-trace/72592
 #jsondata = JSON.parsefile("13tokyo500m.geojson")
 
-fig = Plot(choroplethmapbox(df, geojson = url,
-    featureidkey = "properties.code",
-    locations = df.code,
-    z = df.cluster,
-    colorscale=dcolorscale,
-    colorbar=attr(title="cluster"),
-    height = 600,
-    width = 1000,
-    marker=attr(opacity=0.5),
-    margin = attr(t=10, b=10, l=20, r=20),
+fig = Plot(choroplethmapbox(df, geojson=url,
+        featureidkey="properties.code",
+        locations=df.code,
+        z=df.cluster,
+        colorscale=dcolorscale,
+        colorbar=attr(title="cluster"),
+        height=600,
+        width=1000,
+        marker=attr(opacity=0.5),
+        margin=attr(t=10, b=10, l=20, r=20),
     ),
-    Layout(mapbox = attr(center = attr(lat = 35.7, lon=139.45),
+    Layout(mapbox=attr(center=attr(lat=35.7, lon=139.45),
         zoom=9.1,
         style="open-street-map",
-        )))
+    )))
 
--- map-2.jl
+++ map-2.jl
@@ -6,14 +6,14 @@ function discrete_colorscale(boundary_vals, mycolors)
     #boundary_vals - vector of values bounding intervals/ranges of interest
     #colors - list of rgb or hex colorcodes for values in [bvals[k], bvals[k+1]],1<=k < length(boundary_vals)
     #returns the plotly  discrete colorscale
-    if length(boundary_vals) != length(mycolors)+1
-           error("length of boundary values should be equal to  length(mycolors)+1")
+    if length(boundary_vals) != length(mycolors) + 1
+        error("length of boundary values should be equal to  length(mycolors)+1")
     end
     bvals = sort(boundary_vals)
-    nvals = [(v-bvals[1])/(bvals[end]-bvals[1]) for v in bvals]  #normalized values
-    
+    nvals = [(v - bvals[1]) / (bvals[end] - bvals[1]) for v in bvals]  #normalized values
+
     dcolorscale = [] #discrete colorscale
-    for k in 1:length(mycolors)
+    for k in 1:eachindex(mycolors)
         append!(dcolorscale, [[nvals[k], mycolors[k]], [nvals[k+1], mycolors[k]]])
     end
 
@@ -22,7 +22,7 @@ end
 
 df = GeoDataFrames.read("conbini_tokyo.csv")
 const n_clusters = 10
-df = df[!,2:end]
+df = df[!, 2:end]
 
 # PlotlyJS(julia)では多分discreteがないのでコンビニの名前を数字に変換
 value_map = Dict(i for i in zip(sort(collect(Set(df.idxmax))), Vector(1:n_clusters)))
@@ -38,20 +38,20 @@ dcolorscale = discrete_colorscale(Vector(0:n_clusters), cplotly[1:n_clusters])
 url = "https://www.geospatial.jp/ckan/dataset/ee0203ee-d526-419c-b894-950b03a1ecd0/resource/9f93ab0c-a157-4408-b492-41c1e036eaac/download/13tokyo1km.geojson"
 
 
-fig = Plot(choroplethmapbox(df, geojson = url,
-    #geojson = jsondata,
-    featureidkey = "properties.code",
-    locations = df.mesh_cd,
-    z = df.no,
-    text = df.idxmax,
-    colorscale=dcolorscale,
-    colorbar=attr(title="cluster"),
-    height = 600,
-    width = 1000,
-    marker=attr(opacity=0.5),
-    margin = attr(t=10, b=10, l=20, r=20),
+fig = Plot(choroplethmapbox(df, geojson=url,
+        #geojson = jsondata,
+        featureidkey="properties.code",
+        locations=df.mesh_cd,
+        z=df.no,
+        text=df.idxmax,
+        colorscale=dcolorscale,
+        colorbar=attr(title="cluster"),
+        height=600,
+        width=1000,
+        marker=attr(opacity=0.5),
+        margin=attr(t=10, b=10, l=20, r=20),
     ),
-    Layout(mapbox = attr(center = attr(lat = 35.7, lon=139.45),
+    Layout(mapbox=attr(center=attr(lat=35.7, lon=139.45),
         zoom=9.1,
         style="open-street-map",
-        )))
+    )))
